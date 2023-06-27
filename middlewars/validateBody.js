c2f0a9da3ec7973c2ba4616f8773c2ddec8535bc
Loki99911 <nikita_kresik@i.ua>
2023-06-27 09:06:45 +0300

    Fix Validate body

--- middlewars/validateBody.js
+++ middlewars/validateBody.js
@@ -2,8 +2,11 @@ const { json } = require("body-parser");
 const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
+  
   const func = (req, res, next) => {
-    req.body.additionalAttributes = JSON.parse(req.body.additionalAttributes);
+    if (req.body.hasOwnProperty("additionalAttributes")) {
+      req.body.additionalAttributes = JSON.parse(req.body.additionalAttributes);
+    }
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
