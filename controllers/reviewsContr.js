2c5ea5723ea850c67d8b82ae0a10782b6ba433a6
Loki99911 <nikita_kresik@i.ua>
2023-07-12 15:17:06 +0300

    123

--- controllers/reviewsContr.js
+++ controllers/reviewsContr.js
@@ -3,8 +3,6 @@ const { Review } = require("../models/review");
 
 const addReview = async (req, res) => {
   const lastDate = Date.now();
-  console.log({ ...req.body, lastDate: lastDate });
-  console.log(lastDate);
   const answer = await Review.create({ ...req.body, lastDate: lastDate });
   res.status(201).json(answer);
 };
